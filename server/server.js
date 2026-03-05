f2ba51e67a6825f570870a165862fca62a83a02c
aliDibrahim <aliebraheem2192001@gmail.com>
2026-03-05 12:39:20 +0300

    initial commit

---
+++ server/server.js
@@ -0,0 +1,24 @@
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+import { clerkMiddleware } from '@clerk/express'
+import { serve } from "inngest/express";
+import { inngest, functions } from "./inngest/index.js";
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(clerkMiddleware())
+
+app.get("/", (req, res) => {
+  res.send("server is live.");
+});
+
+// Set up the "/api/inngest" (recommended) routes with the serve handler
+app.use("/api/inngest", serve({ client: inngest, functions }));
+
+const PORT = process.env.PORT || 5500;
+
+app.listen(PORT, () => {
+  console.log(`server is running on port ${5500}`);
+});
